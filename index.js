8699b0781cee8470f1f17b5940dc2f036beb6e95
MarioSimou <marios.simouu@gmail.com>
2023-12-01 21:41:39 +0200

    feat: v1.0.3

--- index.js
+++ index.js
@@ -36,23 +36,22 @@ export default class ServerlessOfflineLambdaFunctionUrls {
   }
   getEvents(functions) {
     const stage = this.getStage()
-    const verbs = this.configuration?.custom?.['serverless-offline']?.urlLambdaFunctionsHttpVerbs ?? [
-      'GET',
-      'POST'
-    ]
+    const verbs = this.configuration?.custom?.['serverless-offline']?.urlLambdaFunctionsHttpVerbs ?? ['GET', 'POST']
     return Object.entries(functions).reduce((events, [functionKey, {handler}]) => {
       const path = `/${stage}/${encodeURIComponent(functionKey)}`
-      return events.concat(verbs.map(v => ({
-        functionKey,
-        handler,
-        http: {
-          routeKey: `${verb} ${path}`,
-          payload: '2.0',
-          isHttpApi: true,
-          path,
-          method: verb
-        }
-      })))
+      return events.concat(
+        verbs.map(verb => ({
+          functionKey,
+          handler,
+          http: {
+            routeKey: `${verb} ${path}`,
+            payload: '2.0',
+            isHttpApi: true,
+            path,
+            method: verb,
+          },
+        }))
+      )
     }, [])
   }
   getStage() {
